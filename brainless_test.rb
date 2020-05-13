74d34371a4f086ab82755e990e40c7f031a2cce6
KwanjiChoi <kwanji0912@gmail.com>
2020-05-14 03:14:25 +0900

    readme

--- brainless_test.rb
+++ brainless_test.rb
@@ -1,7 +1,10 @@
 require 'minitest/autorun'
-
+require './responder.rb'
+require './brainless.rb'
 class BrainlessTest < Minitest::Test
   def test_sample
+    res = WhatResponder.new('what')
     assert_equal 'RUBY','ruby'.upcase
+    assert_equal res.response("こんにちは"),"こんにちはってなぁに？"
   end
 end
